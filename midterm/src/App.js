91b205c332e3bb1d2ea9d5deef74a638d651c70c
bibi <youbinking@naver.com>
2024-04-24 11:16:21 +0900

    두 번째 커밋

---
+++ midterm/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './Middle_Test/Midterm';
+
+function App() {
+  return (
+    <div className="App">
+    <Midterm />
+    </div>
+  );
+}
+
+export default App;
